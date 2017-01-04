25f0147b2e9146a45a3ecc5f370ec084059d4229
Dustin Jenkins <Dustin.Jenkins@nrc-cnrc.gc.ca>
2017-01-04 15:47:43 -0800

    WebRT 73747: Update to make shared library.

---
+++ search-server-lib/src/main/java/ca/nrc/cadc/search/PackageServlet.java
@@ -0,0 +1,162 @@
+/*
+ ************************************************************************
+ *******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+ **************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+ *
+ *  (c) 2015.                            (c) 2015.
+ *  Government of Canada                 Gouvernement du Canada
+ *  National Research Council            Conseil national de recherches
+ *  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+ *  All rights reserved                  Tous droits réservés
+ *
+ *  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+ *  expressed, implied, or               énoncée, implicite ou légale,
+ *  statutory, of any kind with          de quelque nature que ce
+ *  respect to the software,             soit, concernant le logiciel,
+ *  including without limitation         y compris sans restriction
+ *  any warranty of merchantability      toute garantie de valeur
+ *  or fitness for a particular          marchande ou de pertinence
+ *  purpose. NRC shall not be            pour un usage particulier.
+ *  liable in any event for any          Le CNRC ne pourra en aucun cas
+ *  damages, whether direct or           être tenu responsable de tout
+ *  indirect, special or general,        dommage, direct ou indirect,
+ *  consequential or incidental,         particulier ou général,
+ *  arising from the use of the          accessoire ou fortuit, résultant
+ *  software.  Neither the name          de l'utilisation du logiciel. Ni
+ *  of the National Research             le nom du Conseil National de
+ *  Council of Canada nor the            Recherches du Canada ni les noms
+ *  names of its contributors may        de ses  participants ne peuvent
+ *  be used to endorse or promote        être utilisés pour approuver ou
+ *  products derived from this           promouvoir les produits dérivés
+ *  software without specific prior      de ce logiciel sans autorisation
+ *  written permission.                  préalable et particulière
+ *                                       par écrit.
+ *
+ *  This file is part of the             Ce fichier fait partie du projet
+ *  OpenCADC project.                    OpenCADC.
+ *
+ *  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+ *  you can redistribute it and/or       vous pouvez le redistribuer ou le
+ *  modify it under the terms of         modifier suivant les termes de
+ *  the GNU Affero General Public        la “GNU Affero General Public
+ *  License as published by the          License” telle que publiée
+ *  Free Software Foundation,            par la Free Software Foundation
+ *  either version 3 of the              : soit la version 3 de cette
+ *  License, or (at your option)         licence, soit (à votre gré)
+ *  any later version.                   toute version ultérieure.
+ *
+ *  OpenCADC is distributed in the       OpenCADC est distribué
+ *  hope that it will be useful,         dans l’espoir qu’il vous
+ *  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+ *  without even the implied             GARANTIE : sans même la garantie
+ *  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+ *  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+ *  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+ *  General Public License for           Générale Publique GNU Affero
+ *  more details.                        pour plus de détails.
+ *
+ *  You should have received             Vous devriez avoir reçu une
+ *  a copy of the GNU Affero             copie de la Licence Générale
+ *  General Public License along         Publique GNU Affero avec
+ *  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+ *  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+ *                                       <http://www.gnu.org/licenses/>.
+ *
+ *
+ ************************************************************************
+ */
+
+package ca.nrc.cadc.search;
+
+import ca.nrc.cadc.ApplicationConfiguration;
+import ca.nrc.cadc.auth.AuthMethod;
+import ca.nrc.cadc.reg.Standards;
+import ca.nrc.cadc.reg.client.RegistryClient;
+import ca.nrc.cadc.util.StringUtil;
+import ca.nrc.cadc.web.ConfigurableServlet;
+import org.apache.http.client.utils.URIBuilder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+
+/**
+ * Servlet to redirect a caller to the appropriate place for a single request
+ * download of a single CAOM-2 URI.
+ */
+public class PackageServlet extends ConfigurableServlet
+{
+    /**
+     * Only supported method.  This will accept an ID parameter in the request
+     * to query on.
+     *
+     * @param request               The HTTP Request.
+     * @param response              The HTTP Response.
+     * @throws ServletException     Servlet related errors.
+     * @throws IOException          Any other errors.
+     */
+    @Override
+    protected void doGet(final HttpServletRequest request,
+                         final HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        try
+        {
+            get(request, response, new RegistryClient());
+        }
+        catch (URISyntaxException e)
+        {
+            throw new IOException(e);
+        }
+    }
+
+
+    /**
+     * Handle a GET request with the given Registry client to perform the
+     * lookup.
+     *
+     * @param request               The HTTP Request.
+     * @param response              The HTTP Response.
+     * @param registryClient        The RegistryClient to do lookups.
+     * @throws IOException          Any request access problems.
+     * @throws URISyntaxException   For uri issues.
+     */
+    void get(final HttpServletRequest request,
+             final HttpServletResponse response,
+             final RegistryClient registryClient)
+            throws IOException, URISyntaxException
+    {
+        final URL serviceURL = registryClient.getServiceURL(
+                getServiceID(
+                        ApplicationConfiguration.CAOM2PKG_SERVICE_URI_PROPERTY_KEY,
+                        ApplicationConfiguration.DEFAULT_CAOM2PKG_SERVICE_URI),
+                Standards.PKG_10, AuthMethod.COOKIE);
+
+        final URIBuilder builder = new URIBuilder(serviceURL.toURI());
+
+        final String pkgServiceHost =
+                lookup(ApplicationConfiguration.CAOM2PKG_SERVICE_HOST_PORT_PROPERTY_KEY,
+                       ApplicationConfiguration.DEFAULT_CAOM2PKG_SERVICE_HOST_PORT);
+
+        if (StringUtil.hasText(pkgServiceHost))
+        {
+            final URI metaServiceURI = URI.create(pkgServiceHost);
+
+            builder.setHost(metaServiceURI.getHost()).
+                    setPort(metaServiceURI.getPort());
+        }
+
+        for (final String IDValue : request.getParameterValues("ID"))
+        {
+            builder.addParameter("ID", IDValue);
+        }
+
+        response.sendRedirect(builder.build().toURL().toExternalForm());
+    }
+}
