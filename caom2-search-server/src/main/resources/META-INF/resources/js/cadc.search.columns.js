8ec4ea35a661ee8b21f8a1713d9a33cb8568cfef
Alinga Yeung <Alinga.Yeung@nrc-cnrc.gc.ca>
2017-09-12 14:55:56 -0700

    Story 2166. Make search application endpoint configuration via org.opencadc.search.properties.

--- caom2-search-server/src/main/resources/META-INF/resources/js/cadc.search.columns.js
+++ caom2-search-server/src/main/resources/META-INF/resources/js/cadc.search.columns.js
@@ -11,7 +11,7 @@
             "OBSCORE_RESOLVER_VALUE_KEY": "Char.SpatialAxis.Coverage.Support.Area@Shape1Resolver.value",
             "CAOM2_TARGET_NAME_VALUE_KEY": "Plane.position.bounds@Shape1.value",
             "DETAILS_CSS": "details_tooltip_link",
-            "DATALINK_URL_PREFIX": " /search/datalink",
+            "DATALINK_URL_SUFFIX": "/datalink",
             "columns": {
               "OBSERVATION_URI_UTYPE": "caom2:Observation.uri",
               "OBSERVATION_ID_UTYPE": "caom2:Observation.observationID",
@@ -190,7 +190,7 @@
 
                     // Get the thumbnails and previews from datalink
                     $.ajax({
-                             url: ca.nrc.cadc.search.DATALINK_URL_PREFIX,
+                             url: " " + ca.nrc.cadc.search.services.applicationEndpoint + ca.nrc.cadc.search.DATALINK_URL_SUFFIX,
                              dataType: "xml",
                              data: {
                                id: planeURIValue,
