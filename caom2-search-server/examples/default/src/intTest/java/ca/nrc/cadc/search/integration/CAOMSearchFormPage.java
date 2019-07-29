558931eb3569621814579c5e8d560c4390ff9d42
Helena Jeeves <Helena.Jeeves@nrc-cnrc.gc.ca>
2019-07-29 11:19:52 -0700

    s2508, ta11818: change order of wait on page load, hopefully to allow data train enough time to load before check fails.

--- caom2-search-server/examples/default/src/intTest/java/ca/nrc/cadc/search/integration/CAOMSearchFormPage.java
+++ caom2-search-server/examples/default/src/intTest/java/ca/nrc/cadc/search/integration/CAOMSearchFormPage.java
@@ -76,6 +76,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
+import org.openaq.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
